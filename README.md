b15d82c1f87f96117b744205a502bddf40719a58
Thilo Maier <github@maier.asia>
2022-03-22 01:41:53 +0000

    Add client-side image generation Fixes #6

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # sveltekit-example-html-to-image
 
-In this example I test [html-to-image](https://github.com/bubkoo/html-to-image) for client-side and server-side image generation.
+[Run this example on StackBlitz.](https://stackblitz.com/github/maiertech/sveltekit-example-html-to-image?file=src/routes/index.svelte)
 
-## Test client-side image generation
+In this example I test [html-to-image](https://github.com/bubkoo/html-to-image) for client-side image generation. Choose size and pixel ratio and then press **Render as PNG** to replace the blue div with an image.
 
-- Simple div with some text.
-- Button that triggers download of div as image.
+If you run the example on a retina display and choose pixel ratio 1 the image will look blurry. Choose a higher pixel ratio and the image will look crisp.
 
-## Test server-side image generation
+## Tasks
 
-- Provide a URL to endpoint.
-- Render with jsdom or happy-dom.
-- Create image and return it.
+- [ ] Add button to download image.
+- [ ] Test if html-to-image can be run in an endpoint (with jsdom or happy-dom).
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,92 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { toPng } from 'html-to-image';
+
+	let sizes = [
+		{ width: 400, height: 400, label: '400 by 400' },
+		{ width: 1200, height: 675, label: '1200 by 675' }
+	];
+	// Selected size.
+	let size = sizes[1];
+
+	let ratios = [1, 2, 3];
+	// Selected pixel ratio.
+	let ratio = ratios[0];
+
+	// Node that will be rendered as image.
+	let node;
+
+	// Image src (data URL).
+	let src = '';
+
+	// Toggle to display div with class `.image` as image.
+	let displayImg = false;
+
+	// Render as image.
+	async function render() {
+		src = await toPng(node, { width: size.width, height: size.height, pixelRatio: ratio });
+		displayImg = true;
+	}
+</script>
+
+<div class="controls">
+	<label>
+		Size
+		<select bind:value={size} disabled={displayImg}>
+			{#each sizes as size}
+				<option value={size}>
+					{size.label}
+				</option>
+			{/each}
+		</select>
+	</label>
+	<label>
+		Pixel ratio
+		<select bind:value={ratio} disabled={displayImg}>
+			{#each ratios as ratio}
+				<option value={ratio}>
+					{ratio}
+				</option>
+			{/each}
+		</select>
+	</label>
+	<button on:click={render} disabled={displayImg}>Render as PNG</button>
+	<button on:click={() => (displayImg = false)} disabled={!displayImg}>Undo</button>
+</div>
+
+{#if !displayImg}
+	<div
+		bind:this={node}
+		style:width={`${size.width}px`}
+		style:height={`${size.height}px`}
+		class="image"
+	>
+		<h1>Let's test html-to-image</h1>
+		<p>Press <strong>Render as PNG</strong> to replace the blue box with an image.</p>
+	</div>
+{/if}
+
+<div>
+	{#if displayImg}
+		<img {src} width={size.width} height={size.height} alt="Generated." />
+	{/if}
+</div>
+
+<style>
+	.controls {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 10px;
+		margin-bottom: 10px;
+	}
+
+	.image {
+		box-sizing: border-box;
+		background-color: lightblue;
+		padding: 10px;
+	}
+
+	h1 {
+		margin-top: 0;
+	}
+</style>
